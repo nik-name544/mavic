55f825bfe4bce6771140182eb66a2f4e253a1057
nik-name544 <revenkonik30@gmail.com>
2020-07-08 18:39:37 +0300

    upd_v0.21

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,8 @@
 $(function () {
 
+    $('.products__slider').slick({
+        prevArrow: '<button class="slider-btn slider-left"><img src="images/arrow-left.svg"></button>',
+        nextArrow: '<button class="slider-btn slider-right"><img src="images/arrow-right.svg"></button>'
+    });
+
 });
