71545f144b703fd8ec4d97307e4b371ab55b0716
nik-name544 <revenkonik30@gmail.com>
2020-07-09 15:37:53 +0300

    upd_v0.4

--- src/js/main.js
+++ src/js/main.js
@@ -6,4 +6,10 @@ $(function () {
         infinite: false,
     });
 
+    $('.questions__item-title').on('click', function () {
+
+        $('.questions__item').removeClass('questions__item--active');
+        $(this).parent().addClass('questions__item--active');
+        
+    });
 });
